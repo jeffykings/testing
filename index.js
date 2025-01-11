dd42523e22d6d84e1a5fa6758153650d86db3f36
JK7 <99865958+jeffykings@users.noreply.github.com>
2025-01-11 16:57:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   const heroSearchInput = document.querySelector('.hero input[type="text"]');
 
+  const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+    });
+  
   heroSearchInput.addEventListener('input', () => {
     const searchTerm = heroSearchInput.value.toLowerCase();
     console.log(`Searching for: ${searchTerm}`);
